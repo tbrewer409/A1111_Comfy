44387d3de78a98d83ca51e9d63b011b5dc578ce9
Tim B <tbrewer409@gmail.com>
2025-07-04 19:38:45 -0400

    Sync ComfyUI to workspace

--- scripts/start_comfyui.sh
+++ scripts/start_comfyui.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "ComfyUI: Starting ComfyUI"
-cd /ComfyUI
+cd /workspace/ComfyUI
 export PYTHONUNBUFFERED=1
 source /venv_comfyui/bin/activate
 nohup python3 main.py --listen 0.0.0.0 --port 7860 --extra-model-paths-config extra_model_paths.yaml --cuda-malloc --fp16-unet --fp16-vae --force-channels-last > /workspace/logs/comfyui.log 2>&1 &
